53385a69232a54f7cc03c56ac0b90eeaf5cfd5dc
Antonio Serrano Muñoz <toni.semu@gmail.com>
2023-08-24 21:17:38 +0200

    Update shared library API call

--- exts/semu.robotics.tf/semu/robotics/tf/scripts/ros_tf_listener.py
+++ exts/semu.robotics.tf/semu/robotics/tf/scripts/ros_tf_listener.py
@@ -21,14 +21,12 @@ class TfListener:
         sys.path.append(os.path.join(extension_path, "bin"))
 
         # load shared library
-        current_dir= os.getcwd()
-        os.chdir(extension_path)
         import ros_tf_listener_p
-        os.chdir(current_dir)
-
         self._listener = ros_tf_listener_p.RosTfListener()
+        self._listener.start()
 
     def shutdown(self):
+        self._listener.stop()
         self._listener = None
 
     def get_transforms(self, root_frame_name: str = "world"):
@@ -48,16 +46,12 @@ class TfListener:
         return transforms, relations
 
     def get_transform(self, target_frame, source_frame):
-        return (), "TODO"
-        # try:
-        #     transform = self._lookup_transform(target_frame, source_frame, self._time() if self._use_tf2 else self._time(target_frame, source_frame))
-        # except Exception as e:
-        #     return (), type(e).__name__
-        # if self._use_tf2:
-        #     translation = transform.transform.translation
-        #     rotation = transform.transform.rotation
-        #     transform = ([translation.x, translation.y, translation.z], [rotation.x, rotation.y, rotation.z, rotation.w])
-        # return transform, ""
+        frame = self._listener.get_transform(target_frame, source_frame)
+        if not math.isnan(frame["translation"]["x"]):
+            transform = ([frame["translation"][a] for a in ["x", "y", "z"]],
+                         [frame["rotation"][a] for a in ["x", "y", "z", "w"]])
+            return transform, ""
+        return (), frame["name"]
 
     def reset(self):
         """Clear all tf2_ros::Buffer data
@@ -66,7 +60,7 @@ class TfListener:
         """
         if self._listener:
             carb.log_info("Clear all tf2_ros::Buffer data")
-            self._listener.reset()
+            self._listener.reset_buffer()
 
     def is_ready(self):
         return self._listener != None
