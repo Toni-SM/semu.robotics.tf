91bc30dd8eaed6f8afb5e174c0b60383ff8be5fd
Antonio Serrano Muñoz <toni.semu@gmail.com>
2023-08-24 10:54:19 +0200

    Load and use shared library to compute frame transforms in ROS

--- exts/semu.robotics.tf/semu/robotics/tf/scripts/ros_tf_listener.py
+++ exts/semu.robotics.tf/semu/robotics/tf/scripts/ros_tf_listener.py
@@ -1,116 +1,72 @@
-import time
-import yaml
+import os
+import sys
+import math
 
 import carb
 
 
-def acquire_tf_listener_interface(use_tf2: bool = True):
-    interface = TFListener(use_tf2=use_tf2)
-    return interface
+def acquire_tf_listener_interface(extension_path):
+    return TfListener(extension_path)
 
 def release_tf_listener_interface(interface):
     interface.shutdown()
 
 
-class TFListener:
-    def __init__(self, node_name: str = "ros_tf_listener", use_tf2: bool = True) -> None:
-        self._node_name = node_name
-        self._use_tf2 = use_tf2
-        
+class TfListener:
+    def __init__(self, extension_path: str = "") -> None:
         self._listener = None
+        self._root_frame_name = ""
 
-        if not self._init_ros_node():
-            return
+        sys.setdlopenflags(os.RTLD_GLOBAL | os.RTLD_LAZY)
+        sys.path.append(os.path.join(extension_path, "bin"))
 
-        # tf2 implementation
-        if self._use_tf2:
-            import rospy
-            import tf2_ros
-            self._tf_buffer = tf2_ros.Buffer()
-            self._listener = tf2_ros.TransformListener(self._tf_buffer)
-            # internal methods
-            self._time = rospy.Time
-            self._lookup_transform = self._tf_buffer.lookup_transform
-            self._all_frames_as_yaml = self._tf_buffer.all_frames_as_yaml
-        # tf implementation
-        else:
-            import tf
-            self._listener = tf.TransformListener()
-            # internal methods
-            self._time = self._listener.getLatestCommonTime
-            self._lookup_transform = self._listener.lookupTransform
-            self._all_frames_as_yaml = self._listener._buffer.all_frames_as_yaml
+        # load shared library
+        current_dir= os.getcwd()
+        os.chdir(extension_path)
+        import ros_tf_listener_p
+        os.chdir(current_dir)
 
-    def _init_ros_node(self) -> bool:
-        """Initialize the ROS node
-        """
-        import rospy
-        import rosgraph
-        # check ROS master
-        try:
-            rosgraph.Master("/rostopic").getPid()
-        except:
-            carb.log_warn("ROS master is not running")
-            return False
-        # start ROS node
-        try:
-            rospy.init_node(self._node_name)
-            time.sleep(0.1)
-            carb.log_info("ROS node started ({})".format(self._node_name))
-        except rospy.ROSException as e:
-            carb.log_error("ROS node ({}): {}".format(self._node_name, e))
-            return False
-        return True
+        self._listener = ros_tf_listener_p.RosTfListener()
 
     def shutdown(self):
-        if self._listener:
-            if self._use_tf2:
-                self._listener.unregister()
-            self._listener = None
-        # # shutdown ROS node
-        # import rospy
-        # rospy.signal_shutdown("semu.robotics.tf")
+        self._listener = None
 
-    def get_transforms(self, root_frame: str = "world"):
+    def get_transforms(self, root_frame_name: str = "world"):
+        # set root frame
+        if self._root_frame_name != root_frame_name:
+            self._root_frame_name = root_frame_name
+            self._listener.set_root_frame_name(self._root_frame_name)
+
         relations = []
         transforms = {}
+        for frame in self._listener.get_frames():
+            relations.append((frame["name"], frame["parentName"]))
+            if not math.isnan(frame["translation"]["x"]):
+                transforms[frame["name"]] = ([frame["translation"][a] for a in ["x", "y", "z"]],
+                                             [frame["rotation"][a] for a in ["x", "y", "z", "w"]])
 
-        if self._listener:
-            frames = yaml.load(self._all_frames_as_yaml(), Loader=yaml.SafeLoader)
-            if frames is not None:
-                for frame, info in frames.items():
-                    relations.append((frame, info["parent"]))
-                    try:
-                        transform = self._lookup_transform(root_frame, frame, self._time() if self._use_tf2 else self._time(root_frame, frame))
-                        if self._use_tf2:
-                            translation = transform.transform.translation
-                            rotation = transform.transform.rotation
-                            transform = ([translation.x, translation.y, translation.z], [rotation.x, rotation.y, rotation.z, rotation.w])
-                        transforms[frame] = transform
-                    except:
-                        pass
-        
         return transforms, relations
 
     def get_transform(self, target_frame, source_frame):
-        try:
-            transform = self._lookup_transform(target_frame, source_frame, self._time() if self._use_tf2 else self._time(target_frame, source_frame))
-        except Exception as e:
-            return (), type(e).__name__
-        if self._use_tf2:
-            translation = transform.transform.translation
-            rotation = transform.transform.rotation
-            transform = ([translation.x, translation.y, translation.z], [rotation.x, rotation.y, rotation.z, rotation.w])
-        return transform, ""
+        return (), "TODO"
+        # try:
+        #     transform = self._lookup_transform(target_frame, source_frame, self._time() if self._use_tf2 else self._time(target_frame, source_frame))
+        # except Exception as e:
+        #     return (), type(e).__name__
+        # if self._use_tf2:
+        #     translation = transform.transform.translation
+        #     rotation = transform.transform.rotation
+        #     transform = ([translation.x, translation.y, translation.z], [rotation.x, rotation.y, rotation.z, rotation.w])
+        # return transform, ""
 
     def reset(self):
-        # remove "TF_OLD_DATA ignoring data from the past" warning
+        """Clear all tf2_ros::Buffer data
+
+        Clearing the data stop the warning message "TF_OLD_DATA ignoring data from the past"
+        """
         if self._listener:
-            carb.log_info("Reset TF listener (ROS)")
-            if self._use_tf2:
-                self._tf_buffer.clear()
-            else:
-                self._listener._buffer.clear()
+            carb.log_info("Clear all tf2_ros::Buffer data")
+            self._listener.reset()
 
     def is_ready(self):
         return self._listener != None
